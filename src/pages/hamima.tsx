17e37cd65122f0242c3b2174bd9369cca7d38011
Hamima Dalano <mimadalano30@gmail.com>
2020-11-26 01:41:05 +0800

    added hamima page

---
+++ src/pages/hamima.tsx
@@ -0,0 +1,76 @@
+import {
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardText,
+  MDBCardTitle,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBJumbotron,
+  MDBRow,
+} from "mdbreact"
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function hamima() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <MDBContainer className="mt-5 text-center">
+        <MDBRow>
+          <MDBCol>
+            <MDBJumbotron className="text-center">
+              <MDBCardTitle className="card-title h4 pb-2">
+                <strong>Hamima adventure</strong>
+              </MDBCardTitle>
+
+              <MDBCardImage
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+                className="img-fluid"
+              />
+              <MDBCardBody>
+                <MDBCardTitle className="indigo-text h5 m-4">
+                  Photography
+                </MDBCardTitle>
+                <MDBCardText>
+                  Sed ut perspiciatis unde omnis iste natus sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam.
+                </MDBCardText>
+
+                <MDBCol className="d-flex justify-content-center mt-4" md="12">
+                  <MDBCol md="3" className="d-flex justify-content-around">
+                    <a href="#">
+                      <MDBIcon
+                        fab
+                        icon="linkedin-in"
+                        className="grey-text"
+                        size="lg"
+                      />
+                    </a>
+                    <a href="#">
+                      <MDBIcon
+                        fab
+                        icon="twitter"
+                        className="grey-text"
+                        size="lg"
+                      />
+                    </a>
+                    <a href="#">
+                      <MDBIcon
+                        fab
+                        icon="facebook-f"
+                        className="grey-text"
+                        size="lg"
+                      />
+                    </a>
+                  </MDBCol>
+                </MDBCol>
+              </MDBCardBody>
+            </MDBJumbotron>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </Layout>
+  )
+}
